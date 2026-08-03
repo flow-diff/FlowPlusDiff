587abb22c342c0a59816fae143c0fcf602ab5f5f
flow-diff <back2ms@gmail.com>
2026-08-04 00:21:20 +0700

    Delete FlowPlusDiff_org directory

--- FlowPlusDiff_org/utils/__init__.py
+++
@@ -1,7 +0,0 @@
-from pathlib import Path
-import os
-
-def get_root_dir():
-    return Path(__file__).parent.parent
-# root_dir = Path(__file__).parent.parent
-prefix = os.path.join('datasets', 'processed')
